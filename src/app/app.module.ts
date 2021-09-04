53d0fea981551ef5ffa2adcd8455c44c0ba9c230
kek-Sec <georgepetrakis99@gmail.com>
2021-09-04 12:29:39 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxBootstrapMultiselectModule } from 'ngx-bootstrap-multiselect';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgxBootstrapMultiselectModule,
     FlexLayoutModule,
     BrowserModule,
     AppRoutingModule
